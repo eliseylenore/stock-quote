f72a1dbfecddc3c94c37e83f2bc51688902c8bad
Elise St Hilaire <eliseylenore@gmail.com>
2019-03-07 07:39:04 -0800

    add basic search bar and results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from  './Header';
+import Results from  './Results';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '', 
+      showResults: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    console.log(event.target)
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit(event) {
+    this.setState({ showResults: true })
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header value={this.state.value} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+        <Results value={this.state.value} showResults={this.state.showResults}/>
       </div>
     );
   }
